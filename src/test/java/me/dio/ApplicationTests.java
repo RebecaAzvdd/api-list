7ae06ecb756b2b6c52e56b31b3b0da912d469ab6
Rebeca_Azvdd <rebecazevedo2006@gmail.com>
2025-03-12 17:20:09 -0300

    wip: new services, controllers and openai dependecy

--- src/test/java/me/dio/ApplicationTests.java
+++ src/test/java/me/dio/ApplicationTests.java
@@ -1,4 +1,11 @@
 package me.dio;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
